b09783290405479c6f5b8ee8e8694b13796868ac
natalya16 <137273166+natalya16@users.noreply.github.com>
2023-07-05 15:12:19 +0530

    Add files via upload
    This folder contains HTML CSS and JS file for a calculator

---
+++ script.js
@@ -0,0 +1,34 @@
+let string = "";
+        let btn = document.querySelectorAll('.button')
+        Array.from(btn).forEach((button)=>{
+        button.addEventListener('click', (e)=>{
+            if(e.target.innerHTML == '='){
+                string = eval(string);
+                document.querySelector('input').value = string;
+            }
+            else if(e.target.innerHTML == '×'){
+                string = string + "*";
+                document.querySelector('input').value = string;
+            }
+            else if(e.target.innerHTML == '–'){
+                string = string + "-";
+                document.querySelector('input').value = string;
+            }
+            else if(e.target.innerHTML == '%'){
+                string = string + "/";
+                document.querySelector('input').value = string;
+            }
+            else if(e.target.innerHTML == 'AC'){
+                string = ""
+                document.querySelector('input').value = string;
+            }
+            else if(e.target.innerHTML == 'C'){
+                string = string.substring(0,string.length-1)
+                document.querySelector('input').value = string;
+            }
+            else{
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+            }
+        })
+    })
